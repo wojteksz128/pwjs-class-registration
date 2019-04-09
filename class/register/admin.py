6081825b9c35ba88379bf46d72429c5c1507d6c8
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2019-04-10 01:50:27 +0200

    Add models to webapp

--- class/register/admin.py
+++ class/register/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Lecturer, Student, Subject
 
 # Register your models here.
+
+admin.site.register(Lecturer)
+admin.site.register(Student)
+admin.site.register(Subject)
--- class/register/models.py
+++ class/register/models.py
@@ -1,3 +1,28 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s (%s)" % (self.first_name, self.last_name, self.email)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    album_no = models.IntegerField()
+
+    def __str__(self):
+        return "%s %s (%s)" % (self.first_name, self.last_name, self.album_no)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return "%s\nProwadzący: %s" % (self.name, self.lecturer)
