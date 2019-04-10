22502708cd49d5c87e5eec92b11ff88a1681dcba
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2019-04-11 00:22:27 +0200

    Gets all lectures implemented

--- class/register/views.py
+++ class/register/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, render_to_response
 from . import navbar
+from .models import Lecturer
 
 # Create your views here.
 def main_window(request):
-    return render_to_response('index.html', { 'navItems': navbar.navItems, 'active': navbar.navItems[0] })
+    return render_to_response('index.html', 
+    { 
+        'navItems': navbar.navItems, 
+        'active': navbar.navItems[0] 
+    })
 
 def lectures(request):
-    return render_to_response('lectures.html', { 'navItems': navbar.navItems, 'active': navbar.navItems[1] })
+    return render_to_response('lectures.html', 
+    { 
+        'navItems': navbar.navItems, 
+        'active': navbar.navItems[1], 
+        "lectures": Lecturer.objects.all() 
+    })
 
 def subjects(request):
     return render_to_response('subjects.html', { 'navItems': navbar.navItems, 'active': navbar.navItems[2] })
